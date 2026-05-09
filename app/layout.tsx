c23fcd70d9df64d90d66e2a64486a70fd8ca4780
minidukalhara2@gmail.com <minidukalhara14@gmail.com>
2026-05-09 14:51:44 +0530

    Fixing Error ❤️‍🩹

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+  lang="en" 
+  suppressHydrationWarning 
+  className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+>
+  <body className="min-h-full flex flex-col">
+    {children}
+  </body>
+</html>
   );
 }
